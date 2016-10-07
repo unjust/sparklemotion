4b72b7d358b0faf20e90a5fc5b327ff6776f79b2
ivy <ivy@meetup.com>
2016-10-07 13:41:44 -0400

    maybe the files should be added ;)

---
+++ GruntFile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		'sass': {
+			dist: {
+				files: {
+					'dist/animation.css': 'src/animation.scss',
+					'docs/css/animation.css': 'src/animation.scss',
+					'docs/css/elements.css': 'docs/css/elements.scss'
+				}
+			}
+		},
+		'clean': {
+			css: ["dist/*.css"]
+		},
+		'gh-pages': {
+			options: {
+				base: 'docs'
+			},
+			src: ['**']
+		}
+	});
+
+	//grunt.registerTask('compile', ['clean', 'sass']);
+	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('docs', ['gh-pages']);
+};
