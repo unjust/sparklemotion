b28238afb0d1b79dc5510466aca91f3d711bdfab
ivy <ivy@meetup.com>
2016-10-25 17:43:15 -0400

    preprocess and docs in place

--- GruntFile.js
+++ GruntFile.js
@@ -1,19 +1,43 @@
 module.exports = function(grunt) {
+
 	require('load-grunt-tasks')(grunt);
 
+	const	DOCS_SRC = 'docs/src/',
+			DOCS_DEST = 'docs/dest/';
+
 	grunt.initConfig({
+		package: grunt.file.readJSON('package.json'),
+
 		'sass': {
 			dist: {
 				files: {
 					'dist/animation.css': 'src/animation.scss',
-					'docs/css/animation.css': 'src/animation.scss',
-					'docs/css/elements.css': 'docs/css/elements.scss'
+					'docs/dest/css/animation.css': 'src/animation.scss',
+					'docs/dest/css/elements.css': DOCS_SRC + 'css/elements.scss'
 				}
 			}
 		},
+
 		'clean': {
 			css: ["dist/*.css"]
 		},
+
+		'preprocess': {
+			// preprocess docs
+			options: {
+				context: {
+					'VERSION': '<%= package.version %>',
+					'DESCRIPTION': '<%= package.description %>',
+					'CSS_SASSQUATCH': 'https://meetup.github.io/sassquatch2/bundle/sassquatch.css',
+					'CSS_FONT': 'https://secure.meetupstatic.com/fonts/graphik.css'
+				},
+			},
+			docs: {
+				src: DOCS_SRC + 'index.html',
+				dest: DOCS_DEST + 'index.html'
+			}
+		},
+
 		'gh-pages': {
 			options: {
 				base: 'docs'
@@ -24,5 +48,5 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('compile', ['clean', 'sass']);
 	grunt.registerTask('default', ['compile']);
-	grunt.registerTask('docs', ['gh-pages']);
+	grunt.registerTask('docs', ['compile', 'preprocess', 'gh-pages']);
 };
